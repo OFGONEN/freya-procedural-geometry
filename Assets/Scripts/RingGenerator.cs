0c2362df3a4915d7b3d698db981ae2b226bdf309
ofgonen-adastec <faruk.gonen@adastec.com>
2023-07-14 18:01:25 +0300

    feat(RingGenerator): generate ring with correct normals and UVs

--- Assets/Scripts/RingGenerator.cs
+++ Assets/Scripts/RingGenerator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class RingGenerator : MonoBehaviour
 {
@@ -10,15 +11,77 @@ public class RingGenerator : MonoBehaviour
     [Range(0.01f, 256)]
     public float thickness;
     [Range(3, 256)]
-    public int angularSegments = 3;
+    public int angularSegmentCount = 3;
 
     float RadiusOuter => radiusInner + thickness;
-    int VertexCount => angularSegments * 2;
+    int VertexCount => angularSegmentCount * 2;
     
+// Private
+    Mesh mesh;
+    
+
+    private void Awake()
+    {
+        mesh = new Mesh();
+        mesh.name = "Ring Mesh";
+        
+        GetComponent<MeshFilter>().sharedMesh = mesh; // We can contiune to edit the mesh after this.
+    }
+
+    private void Update() => GenerateMesh();
+
+
+    void GenerateMesh()
+    {
+        mesh.Clear();
+
+        int vertexCount = VertexCount;
+        List<Vector3> vertices = new List<Vector3>(vertexCount);
+        List<Vector3> normals = new List<Vector3>(vertexCount);
+        List<Vector2> uvs = new List<Vector2>(vertexCount);
+
+        for (int i = 0; i < angularSegmentCount + 1; i++)
+        {
+            var t = i / (float)angularSegmentCount;
+            float angleRadians = t * CustomMath.TAU;
+
+            Vector2 direction = CustomMath.GetUnitVectorByAngle(angleRadians);
+            
+            vertices.Add(direction * RadiusOuter);
+            vertices.Add(direction * radiusInner);
+            normals.Add(Vector3.forward);
+            normals.Add(Vector3.forward);
+            uvs.Add(new Vector2(t, 1));
+            uvs.Add(new Vector2(t, 0));
+        }
+
+        List<int> triangleIndices = new List<int>(vertexCount * 3);
+
+        for (int i = 0; i < angularSegmentCount; i++)
+        {
+            var indexRoot = i * 2;
+            var indexInnerRoot = indexRoot + 1;
+            var indexOuterNext = (indexRoot + 2);
+            var indexInnerNext = (indexRoot + 3);
+            
+            triangleIndices.Add(indexRoot);
+            triangleIndices.Add(indexOuterNext);
+            triangleIndices.Add(indexInnerNext);
+            
+            triangleIndices.Add(indexRoot);
+            triangleIndices.Add(indexInnerNext);
+            triangleIndices.Add(indexInnerRoot);
+        }
+        
+        mesh.SetVertices(vertices);
+        mesh.SetTriangles(triangleIndices, 0);
+        mesh.SetNormals(normals);
+        mesh.SetUVs(0, uvs);
+    }
 
     private void OnDrawGizmosSelected()
     {
-        CustomGizmos.DrawWiredCircleVertical(transform.position, transform.rotation, radiusInner, angularSegments);
-        CustomGizmos.DrawWiredCircleVertical(transform.position, transform.rotation, RadiusOuter, angularSegments);
+        CustomGizmos.DrawWiredCircleVertical(transform.position, transform.rotation, radiusInner, angularSegmentCount);
+        CustomGizmos.DrawWiredCircleVertical(transform.position, transform.rotation, RadiusOuter, angularSegmentCount);
     }
 }
